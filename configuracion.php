841fb6882a7b22acce5a22a6f7b3ab0b98b6ea23
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-09-02 15:03:43 -0400

    Configuración

---
+++ configuracion.php
@@ -0,0 +1,4 @@
+<?php
+$directory="productora/";		//Sub Directorio, Ej: "productora/" 
+							//si su sistema e encuentre directamente en el dominio sin ningun subdirectorio dejelo vacio Ej: ""
+?>
