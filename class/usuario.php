1078c17c5316b417c88cc796f35a34af7ca71dc2
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-09-02 15:26:43 -0400

    Corregido Class Usuario

--- class/usuario.php
+++ class/usuario.php
@@ -1,7 +1,7 @@
 <?php
 include_once("bd.php");
 class usuario extends bd{
-	var $tabla="usuario";
+	var $tabla="usuarios";
 	function mostrarDatos($CodUsuario){
 		$this->campos=array("*");
 		return $this->getRecords("CodUsuario=$CodUsuario and Activo=1");
@@ -12,13 +12,12 @@ class usuario extends bd{
 		return $this->getRecords("$menos Activo=1","Paterno,Materno,Nombres");
 	}
 	function actualizarDatos($valores,$CodUsuario){
-		//print_r($valores);
 		return $this->updateRow($valores,"CodUsuario=$CodUsuario");
 	}
 	
 	function loginUsuarios($Usuario,$Password){
-		$this->campos=array("count(*) as Can,CodUsuario,Nivel,Idioma");	
-		return $this->getRecords("Usuario='$Usuario' and Pass='$Password' and Activo=1");
+		$this->campos=array("count(*) as Can,codusuarios,nivel");	
+		return $this->getRecords("usuario='$Usuario' and password=MD5('$Password') and Activo=1");
 	}
 }
 ?>
