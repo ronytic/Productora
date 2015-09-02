f5f9bc8db3cd585a13a66577341b2416e3c89d5a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-09-02 16:01:11 -0400

    Class Menu y Submenu

--- class/submenu.php
+++ class/submenu.php
@@ -3,23 +3,13 @@ include_once("bd.php");
 class submenu extends bd{
 	var $tabla="submenu";
 	function mostrar($Nivel,$Menu){
-		$this->campos=array('Nombre','Url','Imagen',"Internet");
+		$this->campos=array('nombre','url','imagen');
 		switch($Nivel){
-			case "1":{return $this->getRecords(" Admin=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-			case "2":{return $this->getRecords(" Gerente=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-			case "3":{return $this->getRecords(" Administrador=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-			case "4":{return $this->getRecords(" Secretaria=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-			case "5":{return $this->getRecords(" Vendedor=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-	}
-	}
-	function verificar($Directorio,$Nivel){
-		switch($Nivel){
-			case "1":{return $this->getRecords("Url='$Directorio' and Admin=1 and Activo=1","Orden");}break;
-			case "2":{return $this->getRecords("Url='$Directorio' and  Gerente=1 and Activo=1","Orden");}break;
-			case "3":{return $this->getRecords("Url='$Directorio' and  Administrador=1 and Activo=1","Orden");}break;
-			case "4":{return $this->getRecords("Url='$Directorio' and  Secretaria=1 and Activo=1","Orden");}break;
-			case "5":{return $this->getRecords("Url='$Directorio' and  Vendedor=1 and Activo=1","Orden");}break;
-		}
+			case "1":{return $this->getRecords(" superadmin=1 and codmenu=$Menu and activo=1","orden");}break;
+			case "2":{return $this->getRecords(" prensa=1 and codmenu=$Menu and activo=1","orden");}break;
+			case "3":{return $this->getRecords(" produccion=1 and codmenu=$Menu and activo=1","orden");}break;
+			case "4":{return $this->getRecords(" programacion=1 and codmenu=$Menu and activo=1","orden");}break;
+        }
 	}
 }
 ?>
