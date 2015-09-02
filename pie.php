7cc3fdca8daf9bc968d655eea42b374fd8de4276
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-09-02 15:04:11 -0400

    Cabecera  Pie

---
+++ pie.php
@@ -0,0 +1,25 @@
+                   </div>
+<!-- Your Content Goes Here -->
+                </div>
+                <!-- /Page Body -->
+            </div>
+            <!-- /Page Content -->
+        </div>
+        <!-- /Page Container -->
+        <!-- Main Container -->
+
+    </div>
+
+    <!--Basic Scripts-->
+    <script src="js/core/jquery.min.js"></script>
+    <script src="js/core/bootstrap.min.js"></script>
+    <script src="js/core/plugins/jquery.slimscroll.min.js"></script>
+
+    <!--Beyond Scripts-->
+    <script src="js/core/assets/beyond.min.js"></script>
+
+    <!--Page Related Scripts-->
+    <!--Google Analytics::Demo Only-->
+</body>
+<!--  /Body -->
+</html>
