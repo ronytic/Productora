714337be3b56bd75216bfc3a6d46f05d1df475eb
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-11-02 09:37:08 -0400

    Funciones

--- funciones/funciones.php
+++ funciones/funciones.php
@@ -251,14 +251,20 @@ function fecha2Str($fecha="",$t=1,$a="+0 day"){
 		return $fecha;
 	}
 }
-function acortarPalabra($texto,$cantidad=1,$separador=" "){
-	$cantidad--;
-	$datos=array();
-	$separado=explode($separador,$texto);
-	for($i=0;$i<=$cantidad;$i++){
-		array_push($datos,$separado[$i]);
-	}
-	return implode($separador,$datos);
+function recortarTexto($texto, $limite=100,$terminador="..."){   
+    $texto = trim($texto);
+    $texto = strip_tags($texto);
+    $tamano = strlen($texto);
+    $resultado = '';
+    if($tamano <= $limite){
+        return $texto;
+    }else{
+        $texto = substr($texto, 0, $limite);
+        $palabras = explode(' ', $texto);
+        $resultado = implode(' ', $palabras);
+        $resultado .= $terminador;
+    }   
+    return $resultado;
 }
 function hora2Str($fecha,$t=1){
 	if(!empty($fecha) && $fecha!="00:00"){
@@ -311,6 +317,22 @@ function quitarSimbolos($string,$Espacio=true){
     	$string = str_replace(array("\\", "¨", "º", "-", "~","#", "@", "|", "!", "\"","·", "$", "%", "&", "/","(", ")", "?", "'", "¡","¿", "[", "^", "`", "]","+", "}", "{", "¨", "´",">", "< ", ";", ",", ":",".", " "),'',$string);
 	}else{
 		$string = str_replace(array("\\", "¨", "º", "-", "~","#", "@", "|", "!", "\"","·", "$", "%", "&", "/","(", ")", "?", "'", "¡","¿", "[", "^", "`", "]","+", "}", "{", "¨", "´",">", "< ", ";", ",", ":",".",),'',$string);	
+	}
+    return $string;
+}
+function quitarSimbolosArchivo($string,$Espacio=true){
+    $string = trim($string);
+    $string = str_replace(array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),$string);
+    $string = str_replace(array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),$string);
+    $string = str_replace(array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),$string);
+    $string = str_replace(array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),$string);
+    $string = str_replace(array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),$string);
+    $string = str_replace(array('ñ', 'Ñ', 'ç', 'Ç'),array('n', 'N', 'c', 'C',),$string);
+ //Esta parte se encarga de eliminar cualquier caracter extraño
+ 	if($Espacio){
+    	$string = str_replace(array("\\", "¨", "º", "-", "~","#", "@", "|", "!", "\"","·", "$", "%", "&", "/","(", ")", "?", "'", "¡","¿", "[", "^", "`", "]","+", "}", "{", "¨", "´",">", "< ", ";", ",", ":", " "),'',$string);
+	}else{
+		$string = str_replace(array("\\", "¨", "º", "-", "~","#", "@", "|", "!", "\"","·", "$", "%", "&", "/","(", ")", "?", "'", "¡","¿", "[", "^", "`", "]","+", "}", "{", "¨", "´",">", "< ", ";", ",", ":",),'',$string);	
 	}
     return $string;
 }
