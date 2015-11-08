74ee825ad8c124d21ef78aca63114b605aacd01d
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-11-08 08:27:44 -0400

    Descargas

--- class/descargas.php
+++ class/descargas.php
@@ -6,5 +6,9 @@ class descargas extends bd{
         $this->campos=array("count(*) as cantidad");
         return $this->getRecords("codvideo=$codvideo and activo=1");
     }
+     function cantidadDescargasWhere($where){
+        $this->campos=array("count(*) as cantidad");
+        return $this->getRecords("$where and activo=1");
+    }
 }
 ?>
