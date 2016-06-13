d5bbe04b4c3042214c4f269b3d258ec8165e2656
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-06-13 10:02:05 -0400

    Login

--- login/index.php
+++ login/index.php
@@ -1,4 +1,15 @@
 <?php
+session_start();
+include_once("../configuracion.php");
+include_once("../funciones/url.php");
+foreach($_SESSION as $k=>$v){
+	unset($_SESSION[$k]);
+}
+unset($_SESSION["login"]);
+unset($_SESSION["CodUsuarioLog"]);
+unset($_SESSION["Nivel"]);
+unset($_SESSION["LoginSistema"]);
+session_destroy();
 $folder="../";
 ?>
 <!DOCTYPE html>
